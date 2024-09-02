4a008670b1ba3541114d6693c7453198ac2133ca
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-09-02 20:25:02 +0530

    status code changed

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ app.use((req, res, next)=>{
     }
 
     if (!body.first_name || !body.last_name || !body.password || !body.email || !body.phone) {
-        return res.status(200).json({ status: "unsuccessful", error: "All fields must be filled." })
+        return res.status(400).json({ status: "unsuccessful", error: "All fields must be filled." })
     } else if (body.password.length<8) {
-        return res.status(200).json({ status: "unsuccessful", error: "Password must be of minimum 8 char lenght." })
+        return res.status(400).json({ status: "unsuccessful", error: "Password must be of minimum 8 char lenght." })
     }else if(!specialChars.test(body.password)){
-        return res.status(200).json({ status: "unsuccessful", error: "Password must contain a special character." })
+        return res.status(400).json({ status: "unsuccessful", error: "Password must contain a special character." })
     }else if(!containsNumber(body.password)){
-        return res.status(200).json({ status: "unsuccessful", error: "Password must contain a numeric letter." })
+        return res.status(400).json({ status: "unsuccessful", error: "Password must contain a numeric letter." })
     }else if(!containsUppercase(body.password)){
-        return res.status(200).json({ status: "unsuccessful", error: "Password must contain a uppercase letter." })
+        return res.status(400).json({ status: "unsuccessful", error: "Password must contain a uppercase letter." })
     }else if(body.phone.length<10){
-        return res.status(200).json({ status: "unsuccessful", error: "Phone number must of 10 digits long." })
+        return res.status(400).json({ status: "unsuccessful", error: "Phone number must of 10 digits long." })
     }else if(!body.email.includes("@")){
-        return res.status(200).json({ status: "unsuccessful", error: "Please enter a valid email address." })
+        return res.status(400).json({ status: "unsuccessful", error: "Please enter a valid email address." })
     }else if(body.first_name[0]!==body.first_name[0].toUpperCase()){
-        return res.status(200).json({ status: "unsuccessful", error: "First letter of name must be in capital." })
+        return res.status(400).json({ status: "unsuccessful", error: "First letter of name must be in capital." })
     }else if(body.last_name[0]!==body.last_name[0].toUpperCase()){
         return res.status(200).json({ status: "unsuccessful", error: "First letter of last name must be in capital." })
     }
